bb14bcd7078d972a45986b21e6d4421348ab4da1
Yassir Irfan <yassirirfan7@gmail.com>
2021-06-09 20:52:03 +0530

    server.js_update

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 require('dotenv').config()
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(fileUpload());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +15,20 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', function(req, res) {
+  
+  if(req.files == null){
+    res.send("<h1> You haven't uploaded any File </h1>")
+  }
+  
+  let file = req.files.upfile; 
 
+  res.json({
+    name:file.name,
+    type:file.mimetype,
+    size:file.size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
